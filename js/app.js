620eb66752c75188124b8c3f800b246cff9c8b9a
ImAlAmin <alamin.cse97@gmail.com>
2022-12-20 12:34:09 +0600

    fixed grid template of liked div and reported div

--- js/app.js
+++ js/app.js
@@ -37,14 +37,20 @@ const switchTab = (id) => {
         document.getElementById( "reported" ).style.display = "none";
     } else if (id === "liked") {
         document.getElementById( "liked" ).style.display = "block";
-        document.getElementById( "posts" ).style.display = "none";
+        document.getElementById( "liked" ).style.display = "grid";
+        document.getElementById("liked-div").style.display = "block";
+        document.getElementById("posts").style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById("reported-div").style.display = "none";
 
         displayLikedPosts();
     } else {
         document.getElementById( "reported" ).style.display = "block";
+        document.getElementById( "reported" ).style.display = "grid";
+        document.getElementById( "reported-div" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
+        ocument.getElementById( "liked-div" ).style.display = "none";
 
         displayReportedPosts();
     }
